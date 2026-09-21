c8d7b753f5f894ca09457bac7c5796cc1afbca41
Iz4c <izac.dugan@education.nsw.gov.au>
2026-09-21 21:28:13 +1000

    added gentoo support, made it install git
    Added functions for logging messages and implemented environment validation checks for package manager and Git installation.

--- install-wm.sh
+++ install-wm.sh
@@ -14,3 +14,52 @@ print_banner() {
   printf "${BOLD}${CYAN}==============================================================================================${RESET}\n"
   printf "${BOLD}${CYAN}                             Astra Window Manager Installer                                   ${RESET}\n"
   printf "${BOLD}${CYAN}==============================================================================================${RESET}\n"
+}
+
+info() {
+  printf "${BLUE}[i]${RESET} %s\n" "$1"
+}
+
+success() {
+  printf "${GREEN}[W]${RESET} %s\n" "$1"
+}
+
+warn() {
+  printf "${YELLOW}[!]${RESET} %s\n" "$1"
+}
+
+error() {
+  printf "${RED}[X]${RESET} %s\n" "$1"
+  exit 1
+}
+
+# --- Main EXEC Flow ---
+clear
+print_banner
+
+info "Starting environment validation..."
+
+# 1. Check for CORRECT PM
+info "checking for PM"
+if command -v pacman >/dev/null 2>&1; then
+  PM="pacman"
+  success "Package Manager Found: pacman"
+elif command -v emerge >/dev/null 2>&1; then
+  PM="emerge"
+  success "Package Manager Found: emerge"
+else
+  error "This Script Requires an Arch-Based Linux Distro or a Gentoo-Based Linux Distro with EMERGE as a PM"
+fi
+
+# 2. Check if Git is downloaded to prevent IFs
+info "Checking For GIT"
+if command -v git >/dev/null 2>&1; then
+  success "Git Installation Found"
+else
+  warn "Git is missing, running 'sudo {PM} {necessary flags} git {--no-confirm for either PM}"
+  if [ "$PM" = "pacman" ]; then
+    sudo pacman -S --noconfirm git
+  elif [ "$PM" = "emerge" ]; then
+    sudo emerge dev-vcs/git
+  fi
+  
